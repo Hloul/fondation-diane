eb5959e341f2430b32bd2c821e0fc4e638637d85
Wafi Chaar <wafi.chaar@bas.sarl>
2023-06-14 07:44:33 +0300

    Update payment.py

--- payment_areeba/models/payment.py
+++ payment_areeba/models/payment.py
@@ -8,7 +8,8 @@ import requests
 from werkzeug import urls
 
 from odoo import api, fields, models
-from odoo.addons.payment.models.payment_acquirer import ValidationError
+from odoo.addons.payment import ValidationError
+
 from odoo.tools.float_utils import float_compare
 
 
