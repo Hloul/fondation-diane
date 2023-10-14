dc619aa686e0b84e3976d579d5a3398cbc6b3d96
Wafi Chaar <wafi.chaar@bas.sarl>
2023-10-14 19:27:50 +0300

    remove unneeded modules

--- payment_areeba/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from . import controllers
-from . import models
-
-from odoo.addons.payment import reset_payment_provider
-
-
-def uninstall_hook(cr, registry):
-    reset_payment_provider(cr, registry, 'areeba')
