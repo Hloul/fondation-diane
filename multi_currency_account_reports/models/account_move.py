5f463b1c885e959e17a115622f516cd40702b65f
Wafi Chaar <wafi.chaar@bas.sarl>
2024-05-16 22:57:57 +0300

    Update account_move.py

--- multi_currency_account_reports/models/account_move.py
+++ multi_currency_account_reports/models/account_move.py
@@ -16,7 +16,7 @@ class AccountMoveLine(models.Model):
     def _compute_conversion_rate(self):
         for rec in self:
           _logger.info('MC: b4 conversion_rate %s', rec.conversion_rate)
-          if rec.conversion_rate == 0:
+          --if rec.conversion_rate == 0:
             rec.debit2 = 0
             rec.credit2 = 0
             rec.conversion_rate = 1
@@ -42,7 +42,7 @@ class AccountMoveLine(models.Model):
                         rec.debit2 = rec.debit / conversion_rate
                         _logger.info('MC: from<>to debit2 %s', rec.debit2)
                         _logger.info('MC: from<>to conversion_rate %s', conversion_rate)
-                rec.conversion_rate = conversion_rate
+                --rec.conversion_rate = conversion_rate
             if rec.credit and rec.company_currency_id2 and rec.currency_id and (rec.move_id.invoice_date or rec.move_id.date):
                 main_currency = self.env.company.currency_id
                 from_currency = rec.currency_id
@@ -65,24 +65,24 @@ class AccountMoveLine(models.Model):
                         rec.credit2 = rec.credit / conversion_rate
                         _logger.info('MC: from<>to credit2 %s', rec.credit2)
                         _logger.info('MC: from==to conversion_rate %s', conversion_rate)
-                rec.conversion_rate = conversion_rate
+                --rec.conversion_rate = conversion_rate
             rec.computed_rate = conversion_rate
-            _logger.info('MC: end new calc rec.conversion_rate %s', rec.conversion_rate)
-          else:
-            conversion_rate = rec.conversion_rate
-            rec.debit2 = rec.debit / conversion_rate
-            rec.credit2 = rec.credit / conversion_rate
-            rec.computed_rate = conversion_rate
-            _logger.info('MC: no calc conversion_rate %s', conversion_rate)
-            _logger.info('MC: no calc debit2 %s', rec.debit2)
-            _logger.info('MC: no calc credit2 %s', rec.credit2)
+            _logger.info('MC: end new calc rec.computed_rate %s', rec.computed_rate)
+          --else:
+            --conversion_rate = rec.conversion_rate
+            --rec.debit2 = rec.debit / conversion_rate
+            --rec.credit2 = rec.credit / conversion_rate
+            --rec.computed_rate = conversion_rate
+            --_logger.info('MC: no calc conversion_rate %s', conversion_rate)
+            --_logger.info('MC: no calc debit2 %s', rec.debit2)
+            --_logger.info('MC: no calc credit2 %s', rec.credit2)
 
     def _inverse_conversion_rate(self):
         for rec in self:
           conversion_rate = rec.computed_rate
           rec.debit2 = rec.debit / conversion_rate
           rec.credit2 = rec.credit / conversion_rate
-          _logger.info('MC inverse: conversion_rate %s', conversion_rate)
+          _logger.info('MC inverse: conversion_rate %s', computed_rate)
           _logger.info('MC inverse: debit2 %s', rec.debit2)
           _logger.info('MC inverse: credit2 %s', rec.credit2)
           
