d9a2aaad3cd8e6321ff756a235e8d313737e64fa
Wafi Chaar <wafi.chaar@bas.sarl>
2024-05-17 09:58:33 +0300

    Update account_move.py

--- multi_currency_account_reports/models/account_move.py
+++ multi_currency_account_reports/models/account_move.py
@@ -5,9 +5,8 @@ _logger = logging.getLogger(__name__)
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    company_currency_id2 = fields.Many2one(string='Second Company Currency', related='company_id.currency_id2',
-                                          readonly=True)
-    computed_rate = fields.Float(string='Computed Rate', compute='_compute_conversion_rate', inverse='_inverse_conversion_rate', store=True, copy=False)
+    company_currency_id2 = fields.Many2one(string='Second Company Currency', related='company_id.currency_id2',readonly=True)
+    computed_rate = fields.Float(string='Computed Rate', compute='_compute_conversion_rate', inverse='_inverse_conversion_rate')
     conversion_rate = fields.Float(string='Conversion Rate', default=0, store=True)
     debit2 = fields.Monetary(string='Debit2', currency_field='company_currency_id2', default=0)
     credit2 = fields.Monetary(string='Credit2', currency_field='company_currency_id2', default=0)
@@ -27,50 +26,52 @@ class AccountMoveLine(models.Model):
                 to_currency = self.env.company.currency_id2
                 if from_currency.id == to_currency.id:
                     conversion_rate = rec.debit / abs(rec.amount_currency)
-                    rec.debit2 = abs(rec.amount_currency)
-                    _logger.info('MC: from==to debit2 %s', rec.debit2)
+                    #rec.debit2 = abs(rec.amount_currency)
+                    #_logger.info('MC: from==to debit2 %s', rec.debit2)
                     _logger.info('MC: from==to conversion_rate %s', conversion_rate)
                 else:
                     if rec.move_id.asset_id:
                         conversion_rate = self.env['res.currency']._get_conversion_rate(
                             to_currency, main_currency, self.env.company, rec.move_id.asset_id.acquisition_date
                         )
-                        rec.debit2 = rec.debit / conversion_rate
+                        #rec.debit2 = rec.debit / conversion_rate
                         _logger.info('MC: asset conversion_rate %s', conversion_rate)
                     else:
                         conversion_rate = self.env['res.currency']._get_conversion_rate(
                             to_currency, main_currency, self.env.company, rec.move_id.invoice_date or rec.move_id.date
                         )
-                        rec.debit2 = rec.debit / conversion_rate
-                        _logger.info('MC: from<>to debit2 %s', rec.debit2)
+                        #rec.debit2 = rec.debit / conversion_rate
+                        #_logger.info('MC: from<>to debit2 %s', rec.debit2)
                         _logger.info('MC: from<>to conversion_rate %s', conversion_rate)
                 #rec.conversion_rate = conversion_rate
                 rec.computed_rate = conversion_rate
+                rec.conversion_rate = rec.conversion_rate
             if rec.credit and rec.company_currency_id2 and rec.currency_id and rec.amount_currency and (rec.move_id.invoice_date or rec.move_id.date):
                 main_currency = self.env.company.currency_id
                 from_currency = rec.currency_id
                 to_currency = self.env.company.currency_id2
                 if from_currency.id == to_currency.id:
                     conversion_rate = rec.credit / abs(rec.amount_currency)
-                    rec.credit2 = abs(rec.amount_currency)
-                    _logger.info('MC: from==to credit2 %s', rec.credit2)
+                    #rec.credit2 = abs(rec.amount_currency)
+                    #_logger.info('MC: from==to credit2 %s', rec.credit2)
                     _logger.info('MC: from==to conversion_rate %s', conversion_rate)
                 else:
                     if rec.move_id.asset_id:
                         conversion_rate = self.env['res.currency']._get_conversion_rate(
                             to_currency, main_currency, self.env.company, rec.move_id.asset_id.acquisition_date
                         )
-                        rec.credit2 = rec.credit / conversion_rate
+                        #rec.credit2 = rec.credit / conversion_rate
                         _logger.info('MC: asset conversion_rate %s', conversion_rate)
                     else:
                         conversion_rate = self.env['res.currency']._get_conversion_rate(
                             to_currency, main_currency, self.env.company, rec.move_id.invoice_date or rec.move_id.date
                         )
-                        rec.credit2 = rec.credit / conversion_rate
+                        #rec.credit2 = rec.credit / conversion_rate
                         _logger.info('MC: from<>to credit2 %s', rec.credit2)
                         _logger.info('MC: from<>to conversion_rate %s', conversion_rate)
                 #rec.conversion_rate = conversion_rate
                 rec.computed_rate = conversion_rate
+                rec.conversion_rate = conversion_rate
             _logger.info('MC: END computed_rate %s', rec.computed_rate)
           #else:
             #conversion_rate = rec.conversion_rate
@@ -84,11 +85,19 @@ class AccountMoveLine(models.Model):
     def _inverse_conversion_rate(self):
         for rec in self:
           conversion_rate = rec.computed_rate
-          rec.debit2 = rec.debit / conversion_rate
-          rec.credit2 = rec.credit / conversion_rate
+          rec.conversion_rate = rec.computed_rate
+          #rec.debit2 = rec.debit / conversion_rate
+          #rec.credit2 = rec.credit / conversion_rate
           _logger.info('MC inverse: conversion_rate %s', conversion_rate)
           _logger.info('MC inverse: computed_rate %s', rec.computed_rate)
-          _logger.info('MC inverse: debit2 %s', rec.debit2)
-          _logger.info('MC inverse: credit2 %s', rec.credit2)
+          #_logger.info('MC inverse: debit2 %s', rec.debit2)
+          #_logger.info('MC inverse: credit2 %s', rec.credit2)
+
+    @api.onchange('conversion_rate')
+    def _onchange_conversion_rate(self):
+         if self.conversion_rate:
+           self.debit2 = self.debit / self.conversion_rate
+           self.credit2 = self.credit / self.conversion
+
           
 
